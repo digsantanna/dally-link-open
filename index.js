d4e5621e0444d41118eff624705cfaf899adccf5
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2022-02-14 19:31:24 -0300

    tranforma conteudo de index.js em função e a chama no proprioarquivo

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const getTaskTime = require('./getTaskTime');
 const hoursNow = require('./hoursNow');
 const getTaskLink = require('./getTaskLink');
 
-const taskTime = getTaskTime();
-const link = getTaskLink();
+const dallyLinkOpen = async () => {
+  const taskTime = getTaskTime();
+  const link = getTaskLink();
 
-console.log(`O link "${link}" será aberto às "${taskTime}"`);
+  console.log(`O link "${link}" será aberto às "${taskTime}"`);
 
-let hour = hoursNow();
+  let hour = hoursNow();
 
-while (taskTime !== hour) {
-  hour = hoursNow();
-  if (hour === taskTime) {
-    handleOpenTask(link);
-    break;
+  while (taskTime !== hour) {
+    hour = hoursNow();
+    if (hour === taskTime) {
+      await handleOpenTask(link);
+      console.log('Encerrando...')
+      break;
+    }
   }
-}
+};
+
+dallyLinkOpen();
